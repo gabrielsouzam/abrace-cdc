2579f73a52390ae3dd0386970c0c2d07311f9740
gabrielmendes64 <gabriel10010100@gmail.com>
2024-03-28 11:25:16 -0300

    Alteração no Header no Layout do app

--- src/pages/_layouts/app.tsx
+++ src/pages/_layouts/app.tsx
@@ -7,10 +7,10 @@ import Logo from './../../assets/logo.svg'
 export function AppLayout() {
   return (
     <div className="h-screen bg-zinc-900 text-zinc-50">
-      <header className="mb-28 flex items-center gap-12 px-20 py-8">
+      <header className="mb-28 flex items-center gap-16 px-20 py-8">
         <img className="h-9 w-10" src={Logo} alt="" />
 
-        <form className="text-zinc-400">
+        <form className="flex-1 text-zinc-400">
           <div
             className="flex items-center gap-2 rounded border-2 
           border-solid border-transparent bg-zinc-800 p-3 focus:border-red-500"
@@ -20,23 +20,33 @@ export function AppLayout() {
             </button>
             <input
               type="text"
-              className="w-96 bg-transparent focus:outline-none "
+              className="w-full bg-transparent focus:outline-none"
               placeholder="Pesquisar por nome da ação, categoria da ação..."
             />
           </div>
         </form>
 
-        <nav className="text-md space-x-8">
-          <Link to="/who-we-are">QUEM SOMOS</Link>
-          <Link to="actions">AÇÕES</Link>
-          <Link to="events">EVENTOS</Link>
-          <Link to="supporters">APOIADORES</Link>
-          <Link to="profile">PERIFL</Link>
+        <nav className="space-x-10 text-xl">
+          <Link className="hover:text-zinc-300" to="/who-we-are">
+            QUEM SOMOS
+          </Link>
+          <Link className="hover:text-zinc-300" to="actions">
+            AÇÕES
+          </Link>
+          <Link className="hover:text-zinc-300" to="events">
+            EVENTOS
+          </Link>
+          <Link className="hover:text-zinc-300" to="supporters">
+            APOIADORES
+          </Link>
+          <Link className="hover:text-zinc-300" to="profile">
+            PERFIL
+          </Link>
         </nav>
 
         <Link
           to="donate"
-          className="flex items-center gap-2 rounded bg-blue-700 px-4 py-2"
+          className="flex items-center gap-2 rounded bg-blue-700 px-5 py-3 text-xl"
         >
           <Heart weight="fill" />
           DOE
