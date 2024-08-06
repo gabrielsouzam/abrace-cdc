54841802be285248724a8850b26693b491ca0692
FelipeOliveira22 <felipe.olv@alu.ufc.br>
2024-08-05 22:11:51 -0300

    feat: creating projects tab

--- src/pages/app/who-we-are/who-we-are.tsx
+++ src/pages/app/who-we-are/who-we-are.tsx
@@ -2,6 +2,8 @@ import { Helmet } from 'react-helmet-async'
 import Lugar from '../../../assets/lugar.svg'
 import Logo from "../../../assets/logo.svg"
 import Completo from "../../../assets/Completo.svg"
+import FeiraPechincha from "../../../assets/feiraPechincha.svg"
+
 export function WhoWeAre() {
   return (
     <>
@@ -10,11 +12,19 @@ export function WhoWeAre() {
         <div className='max-w-lg'>
           <h1 className="text-3xl font-medium text-zinc-50 w-64 top-50 left-20">Quem Somos</h1>
           <div className='mt-6'>
-            <p className='text-start text-base font-normal text-zinc-50 mb-4'>
-              Somos uma plataforma inovadora que une voluntários, doadores e organizações em uma missão comum de transformar o mundo. Nossa paixão por ajudar se encontra com a ação, criando uma comunidade vibrante e dedicada a iniciativas de caridade e arrecadação de fundos. Aqui, acreditamos que cada gesto de generosidade pode causar um impacto significativo e duradouro.
+            <p className='text-justify text-base font-normal text-zinc-50 mb-4'>
+              Somos uma plataforma inovadora que une voluntários, doadores e
+              organizações em uma missão comum de transformar o mundo.
+              Nossa paixão por ajudar se encontra com a ação, criando uma comunidade vibrante
+              e dedicada a iniciativas de caridade e arrecadação de fundos. Aqui,
+              acreditamos que cada gesto de generosidade pode causar um impacto significativo e duradouro.
             </p>
-            <p className='text-start text-base font-normal text-zinc-50'>
-              Em nossa plataforma, conectamos pessoas e instituições que compartilham o desejo de fazer a diferença. Facilitamos a colaboração entre voluntários comprometidos, doadores generosos e organizações que trabalham incansavelmente para atender às necessidades de suas comunidades. Ao unir forças, potencializamos os esforços individuais e coletivos, criando um ambiente onde a solidariedade e a compaixão prosperam.
+            <p className='text-justify text-base font-normal text-zinc-50'>
+              Em nossa plataforma, conectamos pessoas e instituições que compartilham o
+              desejo de fazer a diferença. Facilitamos a colaboração entre voluntários comprometidos,
+              doadores generosos e organizações que trabalham incansavelmente para atender às necessidades
+              de suas comunidades. Ao unir forças, potencializamos os esforços individuais e coletivos,
+              criando um ambiente onde a solidariedade e a compaixão prosperam.
             </p>
           </div>
         </div>
@@ -44,21 +54,49 @@ export function WhoWeAre() {
       </div>
       <div className='relative w-full h-32 flex justify-end right-20'>
         <div className='max-w-lg'>
-          <h1 className='text-3xl font-medium text-zinc-50 relative'>Feira da Pechinha</h1>
+          <h1 className='text-3xl font-medium text-zinc-50 relative text-right'>Feira da Pechinha</h1>
           <div className='mt-6'>
-          <p className='text-start text-base font-normal text-zinc-50 mb-4'>
-              Somos uma plataforma inovadora que une voluntários, doadores e organizações em uma missão comum de transformar o mundo. Nossa paixão por ajudar se encontra com a ação, criando uma comunidade vibrante e dedicada a iniciativas de caridade e arrecadação de fundos. Aqui, acreditamos que cada gesto de generosidade pode causar um impacto significativo e duradouro.
+            <p className='text-justify text-base font-normal text-zinc-50 mb-4'>
+              Somos uma plataforma inovadora que une voluntários, doadores e organizações
+              em uma missão comum de transformar o mundo. Nossa paixão por ajudar se encontra com a ação,
+              criando uma comunidade vibrante e dedicada a iniciativas de caridade e arrecadação de fundos.
+              Aqui, acreditamos que cada gesto de generosidade pode causar um impacto significativo e duradouro.
             </p>
           </div>
         </div>
-        <div className='flex items-start ml-auto'>
-          <img
-            src={Completo}
-            alt='Complemento'
-            className='relative left-64 z-10'
-          />
-          </div>
+
       </div>
+      <div className='flex items-start ml-60 -mt-20'>
+        <img
+          src={FeiraPechincha}
+          alt='Feira Pechincha'
+          className='relative -left-36 -mt-10'
+        />
+      </div>
+      <div className='relative w-full h-32 flex justify-center items-center'>
+      </div>
+      <div className='items-start flex justify-between px-20'>
+        <div className='max-w-lg'>
+          <h1 className='text-3xl font-medium text-zinc-50 relative text-left'>Clube do Idoso</h1>
+          <div className='mt-6'>
+          <p className='text-justify text-base font-normal text-zinc-50 mb-4'>
+              Somos uma plataforma inovadora que une voluntários, doadores e organizações 
+              em uma missão comum de transformar o mundo. Nossa paixão por ajudar se encontra com a ação, 
+              criando uma comunidade vibrante e dedicada a iniciativas de caridade e arrecadação de fundos. 
+              Aqui, acreditamos que cada gesto de generosidade pode causar um impacto significativo e duradouro.
+            </p>
+          </div>
+        </div>
+        <div className='flex items-start ml-60 mt-20'>
+        <img
+          src={FeiraPechincha}
+          alt='Feira Pechincha'
+          className='relative -mt-40'
+        />
+      </div>
+      </div>
+
+
     </>
   )
 }
