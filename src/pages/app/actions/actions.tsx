cbbb92140753ea01d4c67cfb8301f10542460aa1
gabrielmendes64 <gabriel10010100@gmail.com>
2024-09-03 20:56:33 -0300

    feat: add connection for screen action with api

--- src/pages/app/actions/actions.tsx
+++ src/pages/app/actions/actions.tsx
@@ -1,10 +1,66 @@
+import { useEffect, useState } from 'react'
 import { Helmet } from 'react-helmet-async'
 
+import { api } from '../../../lib/axios'
+
+interface Action {
+  id: string
+  title: string
+  subtitle: string
+  addressEntity: {
+    cep: string
+    city: string
+    complement: string
+    id: string
+    number: number
+    road: string
+  }
+  categoryEntity: {
+    description: string
+    id: string
+    name: string
+  }
+  organizerEntity: {
+    id: string
+    name: string
+    email: string
+    cellphone: string
+  }
+  registers: {
+    id: string
+    urlImage: string
+    email: string
+    description: string
+  }[]
+  dateTime: string
+  description: string
+  duration: string
+}
+
 export function Actions() {
+  const [actions, setActions] = useState<Action[]>()
+
+  useEffect(() => {
+    async function getAllActions() {
+      const response = await api.get('/action/')
+      console.log(response)
+      setActions(response.data)
+    }
+
+    getAllActions()
+  }, [])
+
   return (
     <>
       <Helmet title="Actions" />
-      <h1>Actions</h1>
+      <div>
+        {actions?.map((action) => (
+          <div key={action.id} className="text-zinc-900">
+            <h1>Titulo: {action.title}</h1>
+            <span>Subtitulo: {action.subtitle}</span>
+          </div>
+        ))}
+      </div>
     </>
   )
 }
