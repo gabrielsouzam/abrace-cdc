10ce00ff817b98bf984cb2bab75803e8033cb16d
gabrielmendes64 <gabriel10010100@gmail.com>
2024-09-06 16:05:53 -0300

    feat: create screen action and request for an api

--- src/pages/app/actions/actions.tsx
+++ src/pages/app/actions/actions.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react'
 import { Helmet } from 'react-helmet-async'
 
 import { api } from '../../../lib/axios'
+import { ActionCard } from './components/action-card'
 
 interface Action {
   id: string
@@ -43,7 +44,7 @@ export function Actions() {
   useEffect(() => {
     async function getAllActions() {
       const response = await api.get('/action/')
-      console.log(response)
+      console.log(response.data)
       setActions(response.data)
     }
 
@@ -53,13 +54,38 @@ export function Actions() {
   return (
     <>
       <Helmet title="Actions" />
-      <div>
-        {actions?.map((action) => (
-          <div key={action.id} className="text-zinc-900">
-            <h1>Titulo: {action.title}</h1>
-            <span>Subtitulo: {action.subtitle}</span>
-          </div>
-        ))}
+      <div className="mb-20 px-14 text-zinc-900">
+        <div className="mb-11 flex justify-between">
+          <h1 className="text-4xl font-semibold">Ações</h1>
+          <form className="space-x-4">
+            <input
+              placeholder="Categoria"
+              className="rounded border-1 border-zinc-500 bg-transparent px-2 py-1 placeholder-gray-400"
+            />
+            <input
+              placeholder="Bairro"
+              className="rounded border-1 border-zinc-500 bg-transparent px-2 py-1 placeholder-gray-400"
+            />
+          </form>
+        </div>
+
+        <div className="grid w-full grid-cols-3 gap-10">
+          {actions?.map((action) => {
+            return (
+              <ActionCard
+                key={action.id}
+                title={action.title}
+                subtile={action.subtitle}
+                category={action.categoryEntity.name}
+                actionImage={
+                  action.registers.length > 0 && action.registers[0].urlImage
+                    ? action.registers[0].urlImage
+                    : null
+                }
+              />
+            )
+          })}
+        </div>
       </div>
     </>
   )
