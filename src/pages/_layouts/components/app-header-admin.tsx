fd114e8e5b4692300343135abb5ea3af0cc3a6f4
iamcaiorian <caio.rianbr@gmail.com>
2024-09-09 15:53:46 -0300

    fix: header change

--- src/pages/_layouts/components/app-header-admin.tsx
+++ src/pages/_layouts/components/app-header-admin.tsx
@@ -1,73 +1,60 @@
-import { Search } from 'lucide-react'
 import { Link } from 'react-router-dom'
 
 import Logo from './../../../assets/logo.svg'
 
 export function AppHeaderAdmin() {
   return (
-    <header className="mb-28 flex items-center gap-16 px-24 py-8">
+    <header className=" mb-20 flex items-center justify-between border-b-1 border-zinc-200 px-32 py-2">
       <Link to="/">
-        <img className="24" src={Logo} alt="Logo Casa da Caridade" />
+        <img className="24 size-20" src={Logo} alt="Logo Casa da Caridade" />
       </Link>
 
-      <form className="flex-1 text-zinc-400">
-        <div
-          className="flex items-center gap-4 rounded border-2 
-          border-solid border-zinc-500 bg-zinc-50 p-2 px-4 focus:border-red-500"
-        >
-          <button>
-            <Search size={20} />
-          </button>
-          <input
-            type="text"
-            className="text-l w-full bg-transparent focus:outline-none"
-            placeholder="Pesquisar por nome da ação, categoria da ação..."
-          />
-        </div>
-      </form>
-
-      <div className="flex flex-row items-center gap-6">
-        <nav className="text-normal space-x-6">
-          <Link
-            title="quemSomos"
-            className="text-zinc-700 hover:text-zinc-950 active:font-bold"
-            to="/who-we-are"
-          >
-            QUEM SOMOS
-          </Link>
-          <Link
-            className="text-zinc-700 hover:text-zinc-900 active:font-bold"
-            to="actions"
-          >
-            AÇÕES
-          </Link>
-          <Link
-            className="text-zinc-700 hover:text-zinc-900 active:font-bold"
-            to="events"
-          >
-            EVENTOS
-          </Link>
-          <Link
-            className="text-zinc-700 hover:text-zinc-900 active:font-bold"
-            to="supporters"
-          >
-            APOIADORES
-          </Link>
-          <Link
-            className="text-zinc-700 hover:text-zinc-900 active:font-bold"
-            to="profile"
-          >
-            PERFIL
-          </Link>
-        </nav>
-
+      <nav className="text-normal space-x-16">
         <Link
-          to="/admin/create"
-          className="text-l flex items-center gap-2 rounded bg-green-700 px-3 py-2 font-medium"
+          title="quemSomos"
+          className="text-zinc-700 hover:text-zinc-950 active:font-bold"
+          to="/who-we-are"
         >
-          CRIAR
+          QUEM SOMOS
         </Link>
-      </div>
+        <Link
+          className="text-zinc-700 hover:text-zinc-900 active:font-bold"
+          to="actions"
+        >
+          AÇÕES
+        </Link>
+        <Link
+          className="text-zinc-700 hover:text-zinc-900 active:font-bold"
+          to="events"
+        >
+          EVENTOS
+        </Link>
+        <Link
+          className="text-zinc-700 hover:text-zinc-900 active:font-bold"
+          to="supporters"
+        >
+          APOIADORES
+        </Link>
+        <Link
+          className="text-zinc-700 hover:text-zinc-900 active:font-bold"
+          to="supporters"
+        >
+          DOADORES
+        </Link>
+        <Link
+          className="text-zinc-700 hover:text-zinc-900 active:font-bold"
+          to="profile"
+        >
+          PERFIL
+        </Link>
+      </nav>
+
+      <Link
+        to="/admin/create"
+        className="text-l flex items-center gap-2 rounded bg-green-700 px-3 py-2 font-medium text-zinc-50"
+      >
+        CRIAR
+      </Link>
     </header>
   )
 }
