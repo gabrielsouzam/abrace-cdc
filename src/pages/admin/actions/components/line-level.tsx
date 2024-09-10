a0a4c209aabe7a41d7d7a7290302f0023a374a33
gabrielmendes64 <gabriel10010100@gmail.com>
2024-09-10 19:34:18 -0300

    feat: add function for delete an action

--- src/pages/admin/actions/components/line-level.tsx
+++ src/pages/admin/actions/components/line-level.tsx
@@ -4,9 +4,9 @@ interface LineLevelProps {
 
 export function LineLevel({ level }: LineLevelProps) {
   return (
-    <div className="relative h-1 w-[18rem] bg-green-200">
+    <div className="relative h-1 w-[18rem] bg-yellow-200">
       <div
-        className="absolute h-1 bg-green-500"
+        className="absolute h-1 bg-yellow-400"
         style={{ width: `${level}%` }}
       />
     </div>
