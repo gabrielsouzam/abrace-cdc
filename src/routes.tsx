6126aeaf929813d0d10e9df66d89463e2daab186
gabrielmendes64 <gabriel10010100@gmail.com>
2024-09-10 10:30:08 -0300

    fix: change routes name

--- src/routes.tsx
+++ src/routes.tsx
@@ -4,15 +4,12 @@ import { AdminLayout } from './pages/_layouts/admin'
 import { AppLayout } from './pages/_layouts/app'
 import { AuthLayout } from './pages/_layouts/auth'
 import { DonateLayout } from './pages/_layouts/donate'
-
 import { ActionsAdmin } from './pages/admin/actions/actions-admin'
+import { ChooseCreate } from './pages/admin/create/choose-create'
+import { CreateEvent } from './pages/admin/create/create-event/create-event'
 import { Donors } from './pages/admin/donors/donors'
 import { NewAction } from './pages/admin/new-action/new-action'
 import { ActionInfo } from './pages/app/actions/action-info'
-
-import { ChooseCreate } from './pages/admin/create/choose-create'
-import { CreateEvent } from './pages/admin/create/create-event/create-event'
-
 import { Actions } from './pages/app/actions/actions'
 import { ChooseDonation } from './pages/app/donate/choose-donetion'
 import { Donation } from './pages/app/donate/donation/donation'
@@ -69,8 +66,8 @@ export const router = createBrowserRouter([
       { path: 'donors', element: <Donors /> },
       { path: 'new-action', element: <NewAction /> },
       { path: 'actions', element: <ActionsAdmin /> },
-      { path: 'create', element: <ChooseCreate /> },
-      { path: 'create/create-event', element: <CreateEvent /> },
+      { path: 'choose-create', element: <ChooseCreate /> },
+      { path: 'new-event', element: <CreateEvent /> },
     ],
   },
 ])
