c8456df7f82c1d253577cb80fdd45820348120dd
gabrielmendes64 <gabriel10010100@gmail.com>
2024-09-10 10:31:22 -0300

    fix: change route name for create new action and new event

--- src/pages/admin/create/choose-create.tsx
+++ src/pages/admin/create/choose-create.tsx
@@ -13,14 +13,14 @@ export function ChooseCreate() {
 
         <div className="flex items-center gap-16">
           <Link
-            to="/admin/create/create-event"
+            to="/admin/new-event"
             className="flex h-50 w-50 flex-col items-center justify-center gap-8 rounded bg-zinc-50  hover:bg-zinc-300"
           >
             <Plus height="fill" className="h-16 w-18 text-green-500" />
             <span className="font-semibold text-zinc-900">Novo evento</span>
           </Link>
           <Link
-            to="/admin/create"
+            to="/admin/new-action"
             className="flex h-50 w-50 flex-col items-center justify-center gap-8 rounded bg-zinc-50 hover:bg-zinc-300"
           >
             <Plus height="fill" className="h-16 w-18 text-green-500" />
